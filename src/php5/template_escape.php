ad89ab24cdaa8a36edb6ef799d283b6ab08e974f
Emanuele Minotto <minottoemanuele@gmail.com>
2024-11-28 21:48:34 +0100

    use default constants

--- src/php5/template_escape.php
+++ src/php5/template_escape.php
@@ -2,6 +2,6 @@
 
 $deps['template:escape'] = function () {
     return function ($value) {
-        return \htmlspecialchars($value, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+        return \htmlspecialchars($value);
     };
 };
